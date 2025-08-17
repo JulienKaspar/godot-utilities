a602139db42ee87cc05e3bd78bcb723a669cbfd8
JulienKaspar <julien.kaspar@googlemail.com>
2025-08-17 14:16:28 +0200

    Fixed wording of comments and parameters

--- classes/countdown.gd
+++ classes/countdown.gd
@@ -1,8 +1,8 @@
 @icon("res://classes/icons/abacus.svg")
 
 extends Node
-## A custom type for manually ticking down time.
-## An alternative to Timer where there's no explicit stopped state and no pausing.
+## A custom type for manually ticking down of time or other units.
+## An alternative to the Timer where there's no explicit stopped state and no pausing.
 class_name Countdown
 
 @export_range(0.0, 100.0) var MAXIMUM_VALUE : float = 1.0
@@ -13,7 +13,6 @@ var _current_value : float
 
 @onready var _maximum_value := MAXIMUM_VALUE
 
-# TODO: Rewrite comments to not refer to time anymore
 
 func _ready() -> void:
 	if START_DEPLETED:
@@ -22,36 +21,36 @@ func _ready() -> void:
 		_current_value = MAXIMUM_VALUE
 
 
-## Use this to progress the value downwards. Unlike the Timer node, this is an explicit action.
-func progress(elapsed_time : float) -> void:
-	_current_value -= elapsed_time
+## Use this to progress the value towards zero. Unlike the Timer node, this is an explicit action.
+func progress(amount : float) -> void:
+	_current_value -= amount
 	_current_value = max(_current_value, 0.0)
 
 
-## Refill the timeout by a certain amount of seconds.
+## Change the the countdown by a certain amount towards the maximum value.
 ## Disable clamping to overflow the timeout past the maximum value.
-func regress(added_seconds : float, clamp_maximum := true) -> void:
-	_current_value += added_seconds
+func regress(amount : float, clamp_maximum := true) -> void:
+	_current_value += amount
 	if clamp_maximum:
 		_current_value = min(_current_value, _maximum_value)
 
 
-## Reset the timer to the initial value. Optionally set a different maximum time value.
+## Reset the countdown to the initial value. Optionally set a different maximum.
 func reset(maximum_override := _maximum_value) -> void:
 	_current_value = maximum_override
 
 
-## Set the timeout to 0.0.
+## Set the countdown to zero.
 func empty() -> void:
 	_current_value = 0.0
 
 
-## Change the maximum time to a new value in seconds. 
-## Call without arguments to revert to original maximum time.
+## Change the maximum value to something new. 
+## Call without arguments to revert to original maximum.
 func change_maximum_value(new_maximum := MAXIMUM_VALUE) -> void:
 	_maximum_value = new_maximum
 
 
-## Returns true if the timeout has been depleted.
+## Returns true if the countdown reached zero.
 func is_finished() -> bool:
 	return _current_value == 0.0
