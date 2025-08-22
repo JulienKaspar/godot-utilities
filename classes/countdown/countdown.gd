68569e2e23acf411466ea834b02383b2193cfaef
JulienKaspar <julien.kaspar@googlemail.com>
2025-08-22 15:05:15 +0200

    Countdown: Added new features and slight renaming
    There's now a factor for the current value.
    Also added a method to call when creating the node in code to set the export variables easily.

--- classes/countdown/countdown.gd
+++ classes/countdown/countdown.gd
@@ -9,40 +9,63 @@ class_name Countdown
 # TODO: Maybe also add a custom minimum value
 @export var START_DEPLETED := false
 
-var _current_value : float
+## The current value as a 0-1 factor towards the defined maximum value.
+var current_value_factor : float
+## Do not set this value directly.
+## Use the methods "progress", "regress", "reset" and "empty" instead.
+var current_value : float:
+	set(value):
+		current_value = value
+
+		current_value_factor = remap(
+			current_value,
+			0.0,
+			_maximum_value,
+			0.0,
+			1.0
+		)
+		current_value_factor = min(current_value_factor, 1.0)
 
 @onready var _maximum_value := MAXIMUM_VALUE
 
 
 func _ready() -> void:
 	if START_DEPLETED:
-		_current_value = 0.0
+		current_value = 0.0
 	else:
-		_current_value = MAXIMUM_VALUE
+		current_value = MAXIMUM_VALUE
+
+
+## Can be called in case the node is created in code.
+## This will let you set all the usual export variables.
+## IMPORTANT: Call this before adding the instance to the scene, so its ready function is called after.
+func init(init_maximum_value := MAXIMUM_VALUE, init_start_depleted := START_DEPLETED) -> void:	
+	MAXIMUM_VALUE = init_maximum_value
+	START_DEPLETED = init_start_depleted
 
 
 ## Use this to progress the value towards zero. Unlike the Timer node, this is an explicit action.
 func progress(amount : float) -> void:
-	_current_value -= amount
-	_current_value = max(_current_value, 0.0)
+	current_value -= amount
+	current_value = max(current_value, 0.0)
 
 
 ## Change the the countdown by a certain amount towards the maximum value.
 ## Disable clamping to overflow the timeout past the maximum value.
 func regress(amount : float, clamp_maximum := true) -> void:
-	_current_value += amount
+	current_value += amount
 	if clamp_maximum:
-		_current_value = min(_current_value, _maximum_value)
+		current_value = min(current_value, _maximum_value)
 
 
-## Reset the countdown to the initial value. Optionally set a different maximum.
+## Reset the countdown to the initial maximum value. Optionally set a different maximum.
 func reset(maximum_override := _maximum_value) -> void:
-	_current_value = maximum_override
+	current_value = maximum_override
 
 
 ## Set the countdown to zero.
 func empty() -> void:
-	_current_value = 0.0
+	current_value = 0.0
 
 
 ## Change the maximum value to something new. 
@@ -53,4 +76,4 @@ func change_maximum_value(new_maximum := MAXIMUM_VALUE) -> void:
 
 ## Returns true if the countdown reached zero.
 func is_finished() -> bool:
-	return _current_value == 0.0
+	return current_value == 0.0
